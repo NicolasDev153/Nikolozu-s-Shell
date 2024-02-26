cc8e3babf8b76c5f08577a9e17e0d28daee05643
NicolasDev153 <nikaberidze27@gmail.com>
2024-02-26 14:37:49 +0400

    functions for builtin shell command

--- main.c
+++ main.c
@@ -5,9 +5,26 @@
 #include <unistd.h>
 #include <string.h>
 
+//Function Declarations for builtin shell commands
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
-int lsh_launch(char **args)
-{
+//List of builtin commands 
+//followed by their corresponding functions.
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_launch(char **args){
   pid_t pid, wpid;
   int status;
 
