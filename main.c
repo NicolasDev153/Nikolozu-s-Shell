1d71fdd2198bb19382df29c3ea193e7a5f9a9c78
NicolasDev153 <nikaberidze27@gmail.com>
2024-02-26 14:29:08 +0400

    .

--- main.c
+++ main.c
@@ -7,8 +7,21 @@
 
 //reading a line
 #define LSH_RL_BUFSIZE 1024
-char *lsh_read_line(void)
-{
+char *lsh_read_line(void){
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+
   int bufsize = LSH_RL_BUFSIZE;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
