3ce6e8f6de94f78d94cc2d4d814dcdee5df5268f
NicolasDev153 <nikaberidze27@gmail.com>
2024-02-26 14:48:18 +0400

    .

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int lsh_cd(char **args)
 int lsh_help(char **args)
 {
   int i;
-  printf("Stephen Brennan's LSH\n");
+  printf("Nikolozu's Shell\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
@@ -62,7 +62,6 @@ int lsh_exit(char **args)
 {
   return 0;
 }
-
 
 
 int lsh_launch(char **args){
@@ -87,6 +86,26 @@ int lsh_launch(char **args){
   }
 
   return 1;
+}
+
+
+
+int lsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
 }
 
 
